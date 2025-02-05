0a2e6f573d4301acf980b499a12b61743aa0b97e
Jason Tran <anhphantran2002@gmail.com>
2025-02-04 19:27:59 -0600

    added DOM methods and such.

--- script.js
+++ script.js
@@ -14,11 +14,6 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Enter your choice: ");
-    return humanChoice;
-}
-
 function playRound(humanChoice, computerChoice) {
     if (humanChoice.toLowerCase() === "rock") {
         if (computerChoice.toLowerCase() === "rock") {
@@ -53,18 +48,40 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-    
-        console.log(playRound(humanChoice, computerChoice));
-    }
-    if (humanScore > computerScore) {
-        console.log("You win!");
-    } else if (humanScore < computerScore) {
-        console.log("You lose!");
-    }
-}
+const resultDisplay = document.querySelector("body");
+const resultContainer = document.createElement("div");
+resultContainer.classList.add("result-container");
+const humanChoices = document.querySelectorAll(".rps-button");
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+humanChoices.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(playRound(button.textContent, getComputerChoice()));
+        console.log("Human Score: " + humanScore);
+        console.log("Computer Score: " + computerScore);
+        if (humanScore >= 5) {
+            resultContainer.textContent = "You won the game!";
+            resultDisplay.appendChild(resultContainer);
+            resultDisplay.appendChild(resetButton);
+            resetButton.addEventListener("click", () => {
+                humanScore = 0;
+                computerScore = 0;
+                resultContainer.textContent = "";
+                resultDisplay.removeChild(resetButton);
+            }) 
+        } else if (computerScore >= 5) {
+            resultContainer.textContent = "You lost the game!";
+            resultDisplay.appendChild(resultContainer);
+            resultDisplay.appendChild(resultContainer);
+            resultDisplay.appendChild(resetButton);
+            resetButton.addEventListener("click", () => {
+                humanScore = 0;
+                computerScore = 0;
+                resultContainer.textContent = "";
+                resultDisplay.removeChild(resetButton);
+            }) 
+        }
+        // alert(button.id);
+    })
+});
 
-playGame();
